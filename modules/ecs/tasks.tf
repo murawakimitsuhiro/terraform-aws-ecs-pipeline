43b1579c6abf10f26d66cd748e3192e0bade11ed
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2019-03-01 02:12:41 +0900

    Update rds endpoint remove

--- modules/ecs/tasks.tf
+++ modules/ecs/tasks.tf
@@ -2,13 +2,15 @@ data "template_file" "api_task" {
   template = "${file("${path.module}/task-definitions/api-task.json")}"
 
   vars {
-    image                     = "${aws_ecr_repository.web-app.repository_url}"
-    container_name            = "${var.container_name}"
-    container_port            = "${var.container_port}"
-    log_group                 = "${aws_cloudwatch_log_group.web-app.name}"
-    desired_task_cpu          = "${var.desired_task_cpu}"
-    desired_task_memory       = "${var.desired_task_memory}"
-    environment_variables_str = "${replace(join(",",formatlist("{\"name\":%q,\"value\":%q}",keys(var.environment_variables),values(var.environment_variables))), "rds_endpoint", var.db_host_endpoint)}"
+    image               = "${aws_ecr_repository.web-app.repository_url}"
+    container_name      = "${var.container_name}"
+    container_port      = "${var.container_port}"
+    log_group           = "${aws_cloudwatch_log_group.web-app.name}"
+    desired_task_cpu    = "${var.desired_task_cpu}"
+    desired_task_memory = "${var.desired_task_memory}"
+
+    # environment_variables_str = "${replace(join(",",formatlist("{\"name\":%q,\"value\":%q}",keys(var.environment_variables),values(var.environment_variables))), "rds_endpoint", var.db_host_endpoint)}"
+    environment_variables_str = "${join(",",formatlist("{\"name\":%q,\"value\":%q}",keys(var.environment_variables),values(var.environment_variables)))}"
   }
 }
 
