f7776978dbc189c8c47825a4947130a33e32e81a
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2019-04-26 22:21:00 +0900

    Update SSlに対応

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -45,18 +45,17 @@ resource "aws_alb_listener" "web_app" {
   }
 }
 
-#resource "aws_alb_listener" "web_app_ssl" {
-#  count             = "${local.is_ssl ? 1 : 0}"
-#  load_balancer_arn = "${aws_alb.app_alb.arn}"
-#  port              = "443"
-#  protocol          = "HTTPS"
-#  ssl_policy        = "ELBSecurityPolicy-2015-05"
-#
-#  certificate_arn = "${var.ssl_certificate_arn}"
-#
-#  default_action {
-#    target_group_arn = "${aws_alb_target_group.api_target_group.arn}"
-#    type             = "forward"
-#  }
-#}
+resource "aws_alb_listener" "web_app_ssl" {
+  count             = "${local.is_ssl ? 1 : 0}"
+  load_balancer_arn = "${aws_alb.app_alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
 
+  certificate_arn = "${var.ssl_certificate_arn}"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.api_target_group.arn}"
+    type             = "forward"
+  }
+}
