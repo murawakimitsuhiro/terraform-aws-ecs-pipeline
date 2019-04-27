4f1bd37011514b50f54578fea9e460c4c539c659
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2019-04-28 00:47:39 +0900

    Update 443 => 443

--- modules/ecs/security_group.tf
+++ modules/ecs/security_group.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "alb_sg" {
 
   ingress {
     from_port   = "443"
-    to_port     = "${var.container_port}"
+    to_port     = "443"         #"${var.container_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
